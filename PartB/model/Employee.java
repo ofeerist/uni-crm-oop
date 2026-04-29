df3afbff4dfc52bdf1619000f1b746d7254d502f
Artyom <artyomkn@gmail.com>
2026-04-29 09:06:54 +0500

    Update employee

--- PartB/model/Employee.java
+++ PartB/model/Employee.java
@@ -1,12 +1,17 @@
 package PartB.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public abstract class Employee extends User {
 
     private final int employeeId;
     private String position;
+
+    private final List<Message> inbox;
+    private final List<Message> sentMessages;
 
     public Employee(int id, String firstName, String lastName, String email,
                     String password, String phoneNumber, Date registrationDate,
@@ -16,6 +21,9 @@ public abstract class Employee extends User {
 
         this.employeeId = employeeId;
         this.position = position;
+
+        this.inbox = new ArrayList<>();
+        this.sentMessages = new ArrayList<>();
     }
 
     public int getEmployeeId() {
@@ -29,6 +37,15 @@ public abstract class Employee extends User {
     public void setPosition(String position) {
         this.position = position;
     }
+
+     public List<Message> getInbox() {
+        return inbox;
+    }
+
+    public List<Message> getSentMessages() {
+        return sentMessages;
+    }
+
 
     @Override
     public boolean equals(Object o) {
